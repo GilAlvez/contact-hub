0eb29c3521bf74096e51e47c35758e67a7989055
Gil Alves <gildson_af@outlook.com>
2023-11-18 21:54:33 -0300

    feat: update server and add json middleware

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
 
+import Middleware from "./middleware";
 import router from "./routes";
 
 const PORT = 3001;
 const server = express();
+
+server.use(Middleware.json);
 server.use(router);
 
 server.listen(PORT, () => {
