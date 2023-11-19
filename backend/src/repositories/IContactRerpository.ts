5d99a8b31e53aedae14d1a4e5dfb5795a30a545b
Gil Alves <gildson_af@outlook.com>
2023-11-18 21:58:29 -0300

    feat: add contact repository

---
+++ backend/src/repositories/IContactRerpository.ts
@@ -0,0 +1,10 @@
+import { Contact } from "../models/ContactModel";
+import { NewContact } from "../models/NewContactModel";
+
+export interface IContactRepository {
+  findAll: () => Promise<Contact[]>;
+  findById: (id: string) => Promise<Contact | null>;
+  findByEmail: (email: string) => Promise<Contact | null>;
+  create: (contact: NewContact) => void;
+  delete: (id: string) => Promise<void>;
+}
