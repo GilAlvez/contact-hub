8f4ce890f364edaea32192c3f2e35c00017a2205
Gil Alves <gildson_af@outlook.com>
2023-11-25 18:30:07 -0300

    feat: implements async handler to all async routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,21 +2,22 @@ import { Router } from "express";
 
 import CategoryController from "./controllers/CategoryController";
 import ContactController from "./controllers/ContactController";
+import asyncHandler from "./utils/asyncHandler";
 
 const router = Router();
 
 // Contacts
-router.get("/contacts", ContactController.index);
-router.get("/contacts/:id", ContactController.show);
-router.post("/contacts", ContactController.store);
-router.put("/contacts/:id", ContactController.update);
-router.delete("/contacts/:id", ContactController.delete);
+router.get("/contacts", asyncHandler(ContactController.index));
+router.get("/contacts/:id", asyncHandler(ContactController.show));
+router.post("/contacts", asyncHandler(ContactController.store));
+router.put("/contacts/:id", asyncHandler(ContactController.update));
+router.delete("/contacts/:id", asyncHandler(ContactController.delete));
 
 // Categories
-router.get("/categories", CategoryController.index);
-router.get("/categories/:id", CategoryController.show);
-router.post("/categories", CategoryController.store);
-router.put("/categories/:id", CategoryController.update);
-router.delete("/categories/:id", CategoryController.delete);
+router.get("/categories", asyncHandler(CategoryController.index));
+router.get("/categories/:id", asyncHandler(CategoryController.show));
+router.post("/categories", asyncHandler(CategoryController.store));
+router.put("/categories/:id", asyncHandler(CategoryController.update));
+router.delete("/categories/:id", asyncHandler(CategoryController.delete));
 
 export default router;
