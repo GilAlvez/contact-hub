9ba6e72113136f3c906cecadf4f4b18cd9d3a786
GilAlvez <gildson_af@outlook.com>
2023-12-16 11:06:07 -0300

    feat: improve http client solution

--- frontend/src/libs/HttpClient.ts
+++ frontend/src/libs/HttpClient.ts
@@ -1,3 +1,5 @@
+import APIError from "../errors/APIError";
+
 export default class HttpClient {
   constructor(baseURL: string) {
     this.baseUrl = baseURL;
@@ -6,6 +8,19 @@ export default class HttpClient {
   baseUrl: string;
 
   async get(path: string) {
-    return fetch(`${this.baseUrl}${path}`);
+    const response = await fetch(`${this.baseUrl}${path}`);
+
+    let body = null;
+    const contentType = response.headers.get("content-type");
+
+    if (contentType?.includes("json")) {
+      body = await response.json();
+    }
+
+    if (response.ok) {
+      return body;
+    }
+
+    throw new APIError(body?.error ?? `${response.status} - ${response.statusText}`);
   }
 }
