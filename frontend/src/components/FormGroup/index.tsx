8de5bbf97a1324fbdfa2c4adf6a5764613c8af88
GilAlvez <gildson_af@outlook.com>
2023-12-17 22:14:32 -0300

    feat: add loading state to form group

--- frontend/src/components/FormGroup/index.tsx
+++ frontend/src/components/FormGroup/index.tsx
@@ -5,12 +5,20 @@ import * as S from "./styles";
 type FormGroupProps = {
   children: ReactNode;
   errors?: string[];
+  isLoading?: boolean;
 };
 
-export default function FormGroup({ errors, children }: FormGroupProps) {
+export default function FormGroup({ isLoading = false, errors, children }: FormGroupProps) {
   return (
     <S.Container>
-      {children}
+      <S.FormItem>
+        {children}
+        {isLoading && (
+          <S.LoaderWrapper>
+            <S.ItemLoading />
+          </S.LoaderWrapper>
+        )}
+      </S.FormItem>
       {errors?.map((error) => <S.Error>{error}</S.Error>)}
     </S.Container>
   );
