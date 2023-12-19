c545ca546e1765a531763b9398a186d27b6b0a25
GilAlvez <gildson_af@outlook.com>
2023-12-18 21:30:39 -0300

    feat: add unique method to contacts service

--- frontend/src/services/ContactsService.ts
+++ frontend/src/services/ContactsService.ts
@@ -1,20 +1,24 @@
 import { CreateContactFields } from "../components/ContactForm";
 import HttpClient from "../libs/HttpClient";
 
-class ContactService {
+class ContactsService {
   constructor() {
     this.http = new HttpClient("http://localhost:3001");
   }
 
   http: HttpClient;
 
-  async list(orderBy: "ASC" | "DESC" = "ASC") {
+  list(orderBy: "ASC" | "DESC" = "ASC") {
     return this.http.get(`/contacts?orderBy=${orderBy}`);
   }
 
-  async create(contact: CreateContactFields) {
+  unique(id: string) {
+    return this.http.get(`/contacts/${id}`);
+  }
+
+  create(contact: CreateContactFields) {
     return this.http.post("/contacts", { body: JSON.stringify(contact) });
   }
 }
 
-export default new ContactService();
+export default new ContactsService();
