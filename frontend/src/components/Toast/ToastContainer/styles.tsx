951d11227aa105b9fed20b256335531ad204c67e
GilAlvez <gildson_af@outlook.com>
2023-12-18 21:10:45 -0300

    feat: create toasts component

---
+++ frontend/src/components/Toast/ToastContainer/styles.tsx
@@ -0,0 +1,11 @@
+import { styled } from "../../../../styled-system/jsx";
+
+export const Container = styled("div", {
+  base: {
+    zIndex: "10",
+    position: "fixed",
+    bottom: "8",
+    left: "50%",
+    transform: "translateX(-50%)",
+  },
+});
